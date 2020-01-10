dc3fab4ebc47f89df47301995f9f01034ee7ef44
Casasola98 <jgomezcasasola@gmail.com>
2020-01-09 18:38:02 -0600

    Pagina Inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
 import logo from './logo.svg';
+import logoEF from './images/logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Barra">
+        <img src={logoEF} className="ElForaneo" alt="El Foráneo" />
+        <h1>El Foráneo</h1>
+      </div>
+      <div className="App-header">
+        <div className="CuadroUsu">
+          <div className="Superior">
+            <label className="Subtitulo">Inicio de Sesión</label>
+          </div>
+          <div className="InteriorCuadro">
+            <form>
+              <input className="Campo" type="text" id="inputUser" placeholder="Username" required></input>
+              <br />
+              <input className="Campo" type="text" id="inputPass" placeholder="Password" required></input>
+              <br />
+              <button className="Boton">Iniciar Sesión</button>
+            </form>
+          </div>
+        </div>
+        <div className="CuadroReg">
+          <div className="Superior">
+            <label className="Subtitulo">Crea una cuenta</label>
+          </div>
+          <div className="InteriorCuadro2">
+            <form>
+              <input className="Campo" type="text" id="inputNombre" placeholder="Nombre" required></input>
+              <br />
+              <input className="Campo" type="text" id="inputApellido" placeholder="Apellidos" required></input>
+              <br />
+              <input className="Campo" type="text" id="inputEmail" placeholder="Correo" required></input>
+              <br />
+              <input className="Campo" type="text" id="inputUser" placeholder="Username" required></input>
+              <br />
+              <input className="Campo" type="text" id="inputPass" placeholder="Password" required></input>
+              <br />
+              <button className="Boton">Regístrate</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/App2.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          App2 <code>src/App.js</code> Holi.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hola Casa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
