9aec779585bab8b8aa4257926a4d7185a0b83083
Angelo Ramírez Ortega <angeloale1@gmail.com>
2020-01-14 23:43:20 -0600

    Login completo

--- src/App.js
+++ src/App.js
@@ -10,10 +10,19 @@ class App extends React.Component {
     super();
     this.iniciarSesion = this.iniciarSesion.bind(this);
     this.registrarUsuario = this.registrarUsuario.bind(this);
+    this.state = { displayErrors:false, displayErrorsR:false,errorMessage:"",errorMessageR:""};
   }
 
   async iniciarSesion(event) {
     event.preventDefault();
+    if (!event.target.checkValidity()) {
+      // form is invalid! so we do nothing
+      this.setState({ displayErrors: true, displayErrorsR: false,errorMessage:"Por favor revisar los valores insertados",errorMessageR:""});
+      return;
+    }
+    this.state = { displayErrors:false, displayErrorsR:false,errorMessage:"",errorMessageR:""};
+
+    // form is valid! We can parse and submit data
     const formData = new FormData(event.target);
     const data = new URLSearchParams(formData);
     await fetch('/api/login', {
@@ -31,6 +40,9 @@ class App extends React.Component {
           console.log(cookie.get('USER'));
           ReactDOM.render(<Principal />, document.getElementById('root'));
         }
+        else{
+          this.setState({ displayErrors: false, displayErrorsR: false,errorMessage:"Usuario o contraseña invalidos",errorMessageR:""});
+        }
       });
 
   }
@@ -38,6 +50,15 @@ class App extends React.Component {
 
   async registrarUsuario(event) {
     event.preventDefault();
+    if (!event.target.checkValidity()) {
+      // form is invalid! so we do nothing
+      this.setState({ displayErrors: false, displayErrorsR: true});
+      this.setState({ displayErrors: false, displayErrorsR: true,errorMessage:"",errorMessageR:"Por favor revisar los valores insertados"});
+
+      return;
+    }
+    this.setState({ displayErrors: false, displayErrorsR: false});
+    // form is valid! We can parse and submit data
     const formData = new FormData(event.target);
     const data = new URLSearchParams(formData);
 
@@ -55,12 +76,17 @@ class App extends React.Component {
           console.log(cookie.get('USER'));
           ReactDOM.render(<Principal />, document.getElementById('root'));
         }
+        else{
+          this.setState({ displayErrors: false, displayErrorsR: false,errorMessage:"",errorMessageR:"Usuario existente, seleccione un nuevo usuario"});
+        }
       });
 
 
   }
 
   render() {
+    const { displayErrors,displayErrorsR,errorMessage,errorMessageR } = this.state;
+
     return (
       <div id="App" className="App">
         <div className="Barra">
@@ -73,34 +99,36 @@ class App extends React.Component {
               <label className="Subtitulo">Inicio de Sesión</label>
             </div>
             <div className="InteriorCuadro">
-              <form onSubmit={this.iniciarSesion}>
-                <input className="Campo" type="text" name="usuario" id="inputUserR" placeholder="Username" required></input>
+              <form noValidate onSubmit={this.iniciarSesion} className={displayErrors ? 'displayErrors' : ''}>
+                <input className="Campo" type="text" name="usuario" id="inputUserR" placeholder="Username" pattern="^[a-zA-Z0-9_]{1,}$" required></input>
                 <br />
-                <input className="Campo" type="text" name="password" id="inputPassR" placeholder="Password" required></input>
+                <input className="Campo" type="password" name="password" id="inputPassR" placeholder="Password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" required></input>
                 <br />
                 <button className="Boton">Iniciar Sesión</button>
               </form>
             </div>
+            <h3 className="ErrorMessage">{errorMessage}</h3>
           </div>
           <div className="CuadroReg">
             <div className="Superior">
               <label className="Subtitulo Subletra">Crea una cuenta</label>
             </div>
             <div className="InteriorCuadro2">
-              <form onSubmit={this.registrarUsuario}>
-                <input className="Campo" type="text" name="nombre" id="inputNombre" placeholder="Nombre" required></input>
+              <form noValidate onSubmit={this.registrarUsuario} className={displayErrorsR ? 'displayErrors' : ''}>
+                <input className="Campo" type="text" name="nombre" id="inputNombre" placeholder="Nombre" pattern="^[A-Za-zÀ-ÖØ-öø-ÿ]{1,}$" required></input>
                 <br />
-                <input className="Campo" type="text" name="apellidos" id="inputApellido" placeholder="Apellidos" required></input>
+                <input className="Campo" type="text" name="apellidos" id="inputApellido" placeholder="Apellidos" pattern="^[A-Za-zÀ-ÖØ-öø-ÿ ]{1,}$" required></input>
                 <br />
-                <input className="Campo" type="text" name="correo" id="inputEmail" placeholder="Correo" required></input>
+                <input className="Campo" type="email" name="correo" id="inputEmail" placeholder="Correo" required></input>
                 <br />
-                <input className="Campo" type="text" name="usuarioR" id="inputUser" placeholder="Username" required></input>
+                <input className="Campo" type="text" name="usuarioR" id="inputUser" placeholder="Username" pattern="^[a-zA-Z0-9_]{1,}$" required></input>
                 <br />
-                <input className="Campo" type="text" name="passwordR" id="inputPass" placeholder="Password" required></input>
+                <input className="Campo" type="password" name="passwordR" id="inputPass" placeholder="Password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" required></input>
                 <br />
                 <button className="Boton">Regístrate</button>
               </form>
             </div>
+            <h3 className="ErrorMessage">{errorMessageR}</h3>
           </div>
         </div>
       </div>
